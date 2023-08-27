a15a08c76b3b19a05b2131d3413a35aa4209c1f5
Pigensworth <136399546+Pigensworth@users.noreply.github.com>
2023-08-27 17:27:37 -0400

    Create Chat Fix.js

---
+++ Chat Fix.js
@@ -0,0 +1,6 @@
+window.addEventListener('load', function() {
+        setTimeout(function(){
+            geofs.preferences.chat = true;
+            ui.chat.show();
+        }, 3000);
+    }, false);
